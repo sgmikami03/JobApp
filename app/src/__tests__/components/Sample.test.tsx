e1781d4f25741694a176cf074fa9699474f5c7bb
sgmikami03 <sgmikami03@gmail.com>
2024-03-13 07:43:47 +0900

    [add] jest 導入

---
+++ app/src/__tests__/components/Sample.test.tsx
@@ -0,0 +1,10 @@
+import { describe, it } from "@jest/globals";
+import { render } from '@testing-library/react';
+import React from 'react';
+import Sample from "../../components/Sample";
+
+describe("Sample", () => {
+  it("コンポーネントのレンダリングのテスト", async () => {
+    render(<Sample />);
+  });
+});
